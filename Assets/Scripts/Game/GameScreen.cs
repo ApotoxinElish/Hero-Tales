843db0c2b281f2ad4d19b85f6ad3856053db6834
ApotoxinElish <19079181d@connect.polyu.hk>
2023-06-01 18:21:50 +0800

    Import Mirror

---
+++ Assets/Scripts/Game/GameScreen.cs
@@ -0,0 +1,17 @@
+namespace HeroTales
+{
+    public class GameScreen : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
