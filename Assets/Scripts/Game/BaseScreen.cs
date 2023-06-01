2673890c8b2c38a0dc7d8822ec7bfbc0c70c54e4
ApotoxinElish <19079181d@connect.polyu.hk>
2023-06-01 18:36:20 +0800

    import DOTween

--- Assets/Scripts/Game/BaseScreen.cs
+++ Assets/Scripts/Game/BaseScreen.cs
@@ -1,8 +1,10 @@
-using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace HeroTales
 {
-    public class GameScreen : BaseScreen
+    public class BaseScreen : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
